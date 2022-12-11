7d3b2a3da5c1f5c372874db6cc4a8e5571254e56
Kirill <104451159+Modestra@users.noreply.github.com>
2022-12-12 01:46:00 +0700

    12.12.2022

--- ManimInterface/ManimInterface/Form1.Designer.cs
+++ ManimInterface/ManimInterface/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CreateVideoButton = new System.Windows.Forms.Button();
+            this.videoQuality = new System.Windows.Forms.ComboBox();
+            this.qualityLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CreateVideoButton
+            // 
+            this.CreateVideoButton.Location = new System.Drawing.Point(139, 315);
+            this.CreateVideoButton.Name = "CreateVideoButton";
+            this.CreateVideoButton.Size = new System.Drawing.Size(178, 42);
+            this.CreateVideoButton.TabIndex = 0;
+            this.CreateVideoButton.Text = "Создать";
+            this.CreateVideoButton.UseVisualStyleBackColor = true;
+            // 
+            // videoQuality
+            // 
+            this.videoQuality.FormattingEnabled = true;
+            this.videoQuality.Location = new System.Drawing.Point(12, 315);
+            this.videoQuality.Name = "videoQuality";
+            this.videoQuality.Size = new System.Drawing.Size(121, 24);
+            this.videoQuality.TabIndex = 2;
+            // 
+            // qualityLabel
+            // 
+            this.qualityLabel.AutoSize = true;
+            this.qualityLabel.Location = new System.Drawing.Point(12, 286);
+            this.qualityLabel.Name = "qualityLabel";
+            this.qualityLabel.Size = new System.Drawing.Size(112, 16);
+            this.qualityLabel.TabIndex = 3;
+            this.qualityLabel.Text = "Качество видео";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(329, 369);
+            this.Controls.Add(this.qualityLabel);
+            this.Controls.Add(this.videoQuality);
+            this.Controls.Add(this.CreateVideoButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button CreateVideoButton;
+        private System.Windows.Forms.ComboBox videoQuality;
+        private System.Windows.Forms.Label qualityLabel;
     }
 }
 
