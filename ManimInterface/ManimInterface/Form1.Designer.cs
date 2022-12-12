3d3c08fb8702ea5818ff349fbd0e8295d3ab5a80
Kirill <104451159+Modestra@users.noreply.github.com>
2022-12-12 19:09:08 +0700

    12.12.2022

--- ManimInterface/ManimInterface/Form1.Designer.cs
+++ ManimInterface/ManimInterface/Form1.Designer.cs
@@ -31,6 +31,13 @@
             this.CreateVideoButton = new System.Windows.Forms.Button();
             this.videoQuality = new System.Windows.Forms.ComboBox();
             this.qualityLabel = new System.Windows.Forms.Label();
+            this.directoryLink = new System.Windows.Forms.Button();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.labelLink = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // CreateVideoButton
@@ -41,10 +48,14 @@
             this.CreateVideoButton.TabIndex = 0;
             this.CreateVideoButton.Text = "Создать";
             this.CreateVideoButton.UseVisualStyleBackColor = true;
+            this.CreateVideoButton.Click += new System.EventHandler(this.CreateVideoButton_Click);
             // 
             // videoQuality
             // 
             this.videoQuality.FormattingEnabled = true;
+            this.videoQuality.Items.AddRange(new object[] {
+            "Низкое",
+            "Высокое"});
             this.videoQuality.Location = new System.Drawing.Point(12, 315);
             this.videoQuality.Name = "videoQuality";
             this.videoQuality.Size = new System.Drawing.Size(121, 24);
@@ -59,11 +70,78 @@
             this.qualityLabel.TabIndex = 3;
             this.qualityLabel.Text = "Качество видео";
             // 
+            // directoryLink
+            // 
+            this.directoryLink.Location = new System.Drawing.Point(12, 12);
+            this.directoryLink.Name = "directoryLink";
+            this.directoryLink.Size = new System.Drawing.Size(121, 23);
+            this.directoryLink.TabIndex = 4;
+            this.directoryLink.Text = "Путь к папке";
+            this.directoryLink.UseVisualStyleBackColor = true;
+            this.directoryLink.Click += new System.EventHandler(this.directoryLink_Click);
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // labelLink
+            // 
+            this.labelLink.AutoSize = true;
+            this.labelLink.Location = new System.Drawing.Point(12, 38);
+            this.labelLink.Name = "labelLink";
+            this.labelLink.Size = new System.Drawing.Size(44, 16);
+            this.labelLink.TabIndex = 5;
+            this.labelLink.Text = "label1";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 111);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(148, 172);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(166, 111);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(151, 172);
+            this.listView2.TabIndex = 7;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Pythons";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Классы";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(329, 369);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.labelLink);
+            this.Controls.Add(this.directoryLink);
             this.Controls.Add(this.qualityLabel);
             this.Controls.Add(this.videoQuality);
             this.Controls.Add(this.CreateVideoButton);
@@ -79,6 +157,13 @@
         private System.Windows.Forms.Button CreateVideoButton;
         private System.Windows.Forms.ComboBox videoQuality;
         private System.Windows.Forms.Label qualityLabel;
+        private System.Windows.Forms.Button directoryLink;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.Label labelLink;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ManimInterface/ManimInterface/Form1.cs
+++ ManimInterface/ManimInterface/Form1.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace ManimInterface
 {
     public partial class Form1 : Form
     {
+        FolderBrowserDialog fdb = new FolderBrowserDialog();
+        Process p = new Process();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void directoryLink_Click(object sender, EventArgs e)
+        {
+            fdb.ShowDialog();
+            string link = fdb.SelectedPath;
+            labelLink.Text = link;
+
+        }
+
+        private void CreateVideoButton_Click(object sender, EventArgs e)
+        {
+            p.Start();
         }
     }
 }
